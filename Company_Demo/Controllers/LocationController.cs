a1e7417d2e259597c9fdfbbe0a6be56fd20a2001
lenovo <lenovo@DESKTOP-5L0VG9B>
2024-04-08 12:06:12 +0530

    my first commit

---
+++ Company_Demo/Controllers/LocationController.cs
@@ -0,0 +1,53 @@
+﻿using Company_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Company_Demo.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult ShowLoc()
+        {
+            Location loc = new Location();
+            SqlDataReader rdr = loc.GetConnected();
+            ViewBag.ShowDetails = rdr;
+            return View();
+        }
+        public IActionResult DeleteLoc(string locndel)
+        {
+            Location loc = new Location();
+            loc.DeleteLocation(locndel);
+            return RedirectToAction("ShowLoc");
+
+        }
+        [HttpGet]
+        public IActionResult UpdateLoc(string locnupd)
+        {
+            Location loc = new Location();
+            ViewBag.rdr=loc.showUpdate(locnupd);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpdateLoc(string locnid, string locndesc)
+        {
+            Location loc = new Location();
+            loc.updateLocation(locnid, locndesc);
+            return RedirectToAction("ShowLoc");
+        }
+        [HttpGet]
+        public IActionResult InsertLocation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InsertLocation(string locnid,string locndesc)
+        {
+            Location loc = new Location();
+            loc.InsertLocation(locnid,locndesc);
+
+            return RedirectToAction("ShowLoc");
+        }
+
+
+    }
+}
---
+++ Company_Demo/Models/Location.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+namespace Company_Demo.Models
+{
+    public class Location
+    {
+        public SqlConnection conn {  get; set; }
+        
+        public Location()
+        {
+            string connStr = @"Data source=DESKTOP-5L0VG9B\Sandesh;Database=Omkar;Integrated Security=true";
+            conn = new SqlConnection(connStr);
+            conn.Open();
+        }
+        //public SqlConnection GetConnected()
+        public SqlDataReader GetConnected()
+        {
+           // string StrQuery = "select * from Locn_mast";
+            SqlCommand cmd = new SqlCommand("sp_show", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            return cmd.ExecuteReader();
+            
+        }
+        public void DeleteLocation(string locndel)
+        {
+            SqlCommand cmd = new SqlCommand("sp_delete", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@del_code",locndel);
+            cmd.ExecuteNonQuery();
+        }
+        public SqlDataReader showUpdate(string locnupd)
+        {
+            string strQuery="select * from locn_mast where locn_id="+locnupd+"";
+            SqlCommand cmd = new SqlCommand(strQuery, conn);
+            return cmd.ExecuteReader();
+        }
+        public void updateLocation(string locnid,string locndesc)
+        {
+            SqlCommand cmd = new SqlCommand("sp_update", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@locn_code", locnid);
+            cmd.Parameters.AddWithValue("@locn_exp", locndesc);
+            cmd.ExecuteNonQuery();
+
+        }
+        public void InsertLocation(string locnid,string locndesc)
+        {
+            SqlCommand cmd = new SqlCommand("sp_Insert",conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@locn_code", locnid);
+            cmd.Parameters.AddWithValue("@locn_exp", locndesc);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
